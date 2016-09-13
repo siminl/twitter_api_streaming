dd3305a6ec3d848963c2132cff08fd818965ed46
siminl <Simin@dhcp-10-105-118-44.wireless.northwestern.private>
2016-09-13 13:54:17 -0500

    first commit twitter api streaming

---
+++ twitter_streaming.py
@@ -0,0 +1,29 @@
+import tweepy
+from requests_oauthlib import OAuth1Session
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+access_token = "2331702803-xdMRaD87y1NMmVV1GZXkTAa6fi51fiQdUrzNqj1"
+access_token_secret = "1j9f6D3v8mcv1x5BIXqNEsHuxaUkX7vlCejlLATI6NRjW"
+consumer_key = "PnSFkzgj4Ad5tRhwbTKHO0eIO"
+consumer_secret = "mM7V0VWtpiZ0f14npFPCs3IneafrE84ocUe7WhGxnn2BTA94u2"
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['python', 'javascript', 'ruby'])
