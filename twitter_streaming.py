fcd652f501f09fbb9221c486c4daea2ff98a7c31
siminl <Simin@dhcp-10-105-137-171.wireless.northwestern.private>
2016-09-14 16:58:13 -0500

    specified language and keywords time not specified

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -10,7 +10,8 @@ consumer_secret = "mM7V0VWtpiZ0f14npFPCs3IneafrE84ocUe7WhGxnn2BTA94u2"
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
-        print data
+ #       print data
+        with  open('twitter_16_09050909.txt', 'a') as tf: tf.write(data)
         return True
 
     def on_error(self, status):
@@ -23,14 +24,22 @@ if __name__ == '__main__':
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
+ #   stream = Stream(auth, l)
 
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['python', 'javascript', 'ruby'])
+    #This line filter Twitter Streams to capture data by the keywords real-time
+#    stream.filter(track=['IPHONE','iphone','Apple'])
+
+    stream = tweepy.API(auth)
+    for tweet in tweepy.Cursor(stream.search,q= ["IPHONE", "iphone", "Apple", "IP", "ip"], lang ="en").items():
+        print tweet
+#        s = tweet.text
+#       f.write(s)
 
 ######## add in parameter to narrow down the extraction #####
 
-    import json
-    import pandas as pd
-    import matplotlib.pyplot as plt
     
+##
+##    import json
+##    import pandas as pd
+##    import matplotlib.pyplot as plt
+##    
